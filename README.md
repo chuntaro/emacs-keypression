bb9fa9458255893aab4cfb74102e342d06354fc6
chuntaro <chuntaro@sakura-games.jp>
2020-01-07 19:35:25 +0900

    Add setting example

--- README.md
+++ README.md
@@ -19,3 +19,15 @@ M-x keycaster-mode
 ```
 
 Run it again to turn it off.
+
+The default is a simple display, so you can customize it as follows.
+
+```emacs-lisp
+(setq keycaster-use-child-frame nil
+      keycaster-fade-out-delay 1.0
+      keycaster-frame-justify 'keycaster-left-justified
+      keycaster-cast-command-name t
+      keycaster-cast-command-name-format "%s  %s"
+      keycaster-combine-same-keystrokes t
+      keycaster-font-face-attribute '(:width normal :height 200 :weight bold))
+```
--- keycaster.el
+++ keycaster.el
@@ -39,6 +39,16 @@
 ;; M-x keycaster-mode
 ;;
 ;; Run it again to turn it off.
+;;
+;; The default is a simple display, so you can customize it as follows.
+;;
+;; (setq keycaster-use-child-frame nil
+;;       keycaster-fade-out-delay 1.0
+;;       keycaster-frame-justify 'keycaster-left-justified
+;;       keycaster-cast-command-name t
+;;       keycaster-cast-command-name-format "%s  %s"
+;;       keycaster-combine-same-keystrokes t
+;;       keycaster-font-face-attribute '(:width normal :height 200 :weight bold))
 
 ;;; Code:
 
