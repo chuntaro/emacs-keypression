05d0424eec67920dc50bb4c490799118a275378b
chuntaro <chuntaro@sakura-games.jp>
2020-01-09 15:53:13 +0900

    Bump version

--- keycaster.el
+++ keycaster.el
@@ -4,7 +4,7 @@
 
 ;; Author: chuntaro <chuntaro@sakura-games.jp>
 ;; Keywords: key, screencast, tools
-;; Version: 0.9.0
+;; Version: 1.0.0
 ;; Homepage: https://github.com/chuntaro/emacs-keycaster
 ;; Package-Requires: ((emacs "26.3"))
 
